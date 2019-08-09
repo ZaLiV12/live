1b4220f28af4f1d505c6bac85bf0e12fe7dd0816
ZaLiV12 <44550056+ZaLiV12@users.noreply.github.com>
2019-08-09 17:40:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Liuuuuqa ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Sleeping منطمر`,"http://twitch.tv/Liuuuuqa")
+client.user.setGame(`يؤلد آلله نآيم لاتدز رسآيل `,"http://twitch.tv/Liuuuuqa")
 client.user.setStatus("dnd")
 });
 
